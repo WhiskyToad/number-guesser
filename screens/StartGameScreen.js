717b6ae392f46d71f3511379330bab93e2357ef9
Steven Craig <stevencraig321@gmail.com>
2024-02-02 08:18:16 +0000

    feat: number input styling

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType={"number-pad"}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -25,5 +31,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 34,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    marginVertical: 8,
+    color: "#ddb52f",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
