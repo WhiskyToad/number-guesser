010676ae0476bf8e151e3b2ab84b41d20161ffec
Steven Craig <stevencraig321@gmail.com>
2024-02-19 11:56:53 +0000

    feat: basic game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,22 @@
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const GameScreen = () => {
-  return <View>GameScreen</View>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponents guess</Text>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      <View></View>
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    pading: 12,
+  },
+});
