46aa604fff347bdd3a874cfe8fd78f18543a3284
Steven Craig <stevencraig321@gmail.com>
2024-02-19 14:01:31 +0000

    refactor: colors constants

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,4 +1,5 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import Colors from "../constants/colors";
 
 const PrimaryButton = (props) => {
   return (
@@ -10,7 +11,7 @@ const PrimaryButton = (props) => {
             : styles.buttonInnerContainer
         }
         onPress={() => console.log("pressed")}
-        android_ripple={{ color: "#640233" }}
+        android_ripple={{ color: Colors.primary600 }}
       >
         <Text style={styles.buttonText}>{props.children}</Text>
       </Pressable>
@@ -27,7 +28,7 @@ const styles = StyleSheet.create({
     overflow: "hidden",
   },
   buttonInnerContainer: {
-    backgroundColor: "#72063c",
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
