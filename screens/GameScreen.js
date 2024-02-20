1bd88f282d8d8f20b33db32e638560f25e84a467
Steven Craig <stevencraig321@gmail.com>
2024-02-20 15:04:59 +0000

    feat: computer keeps guessing

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import Title from "../components/ui/Title";
 import { useState } from "react";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 const generateRandomNumber = (min, max, exclude) => {
   const randomNum = Math.floor(Math.random() * (max - min) + min);
@@ -16,6 +17,37 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomNumber(1, 100, props.selectedNumber)
   );
+  const [minBoundary, setMinBoundary] = useState(1);
+  const [maxBoundary, setMaxBoundary] = useState(100);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < props.selectedNumber) ||
+      (direction === "higher" && currentGuess > props.selectedNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      setMaxBoundary(currentGuess);
+      const newGuess = generateRandomNumber(
+        minBoundary,
+        currentGuess,
+        currentGuess
+      );
+      setCurrentGuess(newGuess);
+    } else {
+      setMinBoundary(currentGuess + 1);
+      const newGuess = generateRandomNumber(
+        currentGuess,
+        maxBoundary,
+        currentGuess
+      );
+      setCurrentGuess(newGuess);
+    }
+  };
 
   return (
     <View style={styles.screen}>
@@ -23,6 +55,14 @@ const GameScreen = (props) => {
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
+        <View>
+          <PrimaryButton onPress={() => nextGuessHandler("lower")}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={() => nextGuessHandler("higher")}>
+            +
+          </PrimaryButton>
+        </View>
       </View>
       <View></View>
     </View>
